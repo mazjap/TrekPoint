95f24d2388bd636e49884e6ac486b2dd86e8f10f
Jordan Christensen <jordan.c4922@gmail.com>
2024-02-29 16:58:42 -0700

    Initial Commit

---
+++ MakeAMap/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MakeAMap
+//
+//  Created by Jordan Christensen on 2/29/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MakeAMap/MakeAMapApp.swift
@@ -0,0 +1,32 @@
+//
+//  MakeAMapApp.swift
+//  MakeAMap
+//
+//  Created by Jordan Christensen on 2/29/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MakeAMapApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
