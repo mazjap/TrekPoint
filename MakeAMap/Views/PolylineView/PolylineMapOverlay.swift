9e5ee26ef29593ddce47497ce560c71fd44b6d53
Jordan C <jordan.c4922@gmail.com>
2025-03-02 13:04:50 -0700

    Refactor `PolylineMapOverlay` to allow for different styles (drawn path vs location tracked path)

--- MakeAMap/Views/PolylineView/PolylineMapOverlay.swift
+++ MakeAMap/Views/PolylineView/PolylineMapOverlay.swift
@@ -4,10 +4,30 @@ import MapKit
 struct PolylineMapOverlay: MapContent {
     let polyline: any PolylineProvider
     let strokeColor: Color
+    let lineWidth: CGFloat
+    let dashPattern: [CGFloat]?
+    
+    init(
+        polyline: any PolylineProvider,
+        strokeColor: Color,
+        lineWidth: CGFloat = 3,
+        dashPattern: [CGFloat]? = [5, 8]
+    ) {
+        self.polyline = polyline
+        self.strokeColor = strokeColor
+        self.lineWidth = lineWidth
+        self.dashPattern = dashPattern
+    }
     
     var body: some MapContent {
         MapPolyline(coordinates: polyline.clCoordinates)
-            .stroke(strokeColor, style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round, miterLimit: 5, dash: [5, 8]))
+            .stroke(strokeColor, style: StrokeStyle(
+                lineWidth: lineWidth,
+                lineCap: .round,
+                lineJoin: .round,
+                miterLimit: 5,
+                dash: dashPattern ?? []
+            ))
             .tag(polyline.tag)
     }
 }
