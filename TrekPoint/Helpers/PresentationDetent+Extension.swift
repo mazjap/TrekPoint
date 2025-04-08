df7c0cd945736909d99b4f2fd3efa4751b7c5a6b
Jordan C <jordan.c4922@gmail.com>
2025-04-07 22:11:27 -0600

    Implement custom presentation detent to use maximum possible detent size without triggering the built in scale effect

--- TrekPoint/Helpers/PresentationDetent+Extension.swift
+++ TrekPoint/Helpers/PresentationDetent+Extension.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 
+struct MaxPresentationDetentWithoutScaleEffect: CustomPresentationDetent {
+    static func height(in context: Context) -> CGFloat? {
+        context.maxDetentValue - 1
+    }
+}
+
 extension PresentationDetent {
     static let small = Self.fraction(0.15)
-    static let largeWithoutScaleEffect = Self.fraction(0.99)
+    static let largeWithoutScaleEffect = Self.custom(MaxPresentationDetentWithoutScaleEffect.self)
 }
 
 extension Set where Element == PresentationDetent {
