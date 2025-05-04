c7708ebb49f3dfa45935effcfaca7e546eba5349
Jordan C <jordan.c4922@gmail.com>
2025-05-04 11:48:23 -0600

    Use swift-dependencies for `UserDefaultsProvider`, refactor `LocationTrackingManager` to use `@Dependency` macro for `UserDefaultsProvider`, remove userDefaultsProvider parameter from initializer

--- TrekPoint/Managers/UserDefaultsProvider.swift
+++ TrekPoint/Managers/UserDefaultsProvider.swift
@@ -1,8 +1,22 @@
-//
-//  UserDefaultsProvider.swift
-//  TrekPoint
-//
-//  Created by Jordan Christensen on 5/4/25.
-//
-
 import Foundation
+import Dependencies
+
+protocol UserDefaultsProvider {
+    func bool(forKey: String) -> Bool
+    func set(_ value: Any?, forKey: String)
+    func string(forKey: String) -> String?
+    func removeObject(forKey: String)
+}
+
+extension UserDefaults: UserDefaultsProvider {}
+
+enum UserDefaultsProviderKey: DependencyKey {
+    static let liveValue: any UserDefaultsProvider = UserDefaults.standard
+}
+
+extension DependencyValues {
+    var userDefaultsProvider: any UserDefaultsProvider {
+        get { self[UserDefaultsProviderKey.self] }
+        set { self[UserDefaultsProviderKey.self] = newValue }
+    }
+}
